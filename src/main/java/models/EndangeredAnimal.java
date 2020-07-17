0b266cff8371f142c78d639424305ca60bd92abc
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-17 19:35:24 +0300

    Create methods for tests

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 public class EndangeredAnimal extends Animal{
     private String health;
     private String age;
+
     private static final String DATABASE_TYPE = "Endangered Animals";
     public EndangeredAnimal(String name, String health, String age) {
         super(name);
